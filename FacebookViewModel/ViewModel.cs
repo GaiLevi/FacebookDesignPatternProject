b04c4dc904ec019a861b5346f0fdb380a264b91e
Gai Levi <gailevi1414@gmail.com>
2023-06-15 17:54:21 +0300

    duplicate imageIterator fix

--- FacebookViewModel/ViewModel.cs
+++ FacebookViewModel/ViewModel.cs
@@ -165,42 +165,5 @@ of your life in our stupid App!",
             return betterThingToDo;
         }
 
-        public class ImageURLIterator : IEnumerator<string>
-        {
-            private readonly ObservableCollection<string> urlCollection;
-            private int index = -1;
-
-            public ImageURLIterator(ObservableCollection<string> urlCollection)
-            {
-                this.urlCollection = urlCollection;
-            }
-
-            public string Current => urlCollection[index];
-
-            object IEnumerator.Current => Current;
-
-            public void Dispose() { }
-
-            public bool MoveNext()
-            {
-                index++;
-                return (index < urlCollection.Count);
-            }
-
-            public void Reset()
-            {
-                index = -1;
-            }
-
-            public bool MovePrevious()
-            {
-                if (index > 0)
-                {
-                    index--;
-                    return true;
-                }
-                return false;
-            }
-        }
     }
 }
